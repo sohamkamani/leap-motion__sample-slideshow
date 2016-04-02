bf1b5e8e0e01131b4cde49b1e418caa682d1547d
sohamkamani <sohamkamani@gmail.com>
2016-04-02 23:09:09 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+
+const node = document.getElementById('app-node');
+
+ReactDOM.render(
+  <App/>, node
+);
+
